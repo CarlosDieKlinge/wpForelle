529ef2fa7759be84119365be6d2ee374e02d0bb1
CarlosDieKlinge <66059864+CarlosDieKlinge@users.noreply.github.com>
2020-07-01 11:56:55 +0200

    SD
    trf

---
+++ index.php
@@ -0,0 +1,109 @@
+<!-- wp:navigation {"orientation":"horizontal","className":"is-style-dark","itemsJustification":"center","align":"full"} -->
+<!-- wp:navigation-link {"label":"Home ","url":"https://richiezz.wordpress.com/?page_id=29"} /-->
+
+<!-- wp:navigation-link {"label":"Rezepte ","url":"https://richiezz.wordpress.com/?page_id=47"} /-->
+
+<!-- wp:navigation-link {"label":"Zubehör","url":"https://richiezz.wordpress.com/?page_id=55"} /-->
+
+<!-- wp:navigation-link {"label":"Angel-Orte","url":"https://richiezz.wordpress.com/?page_id=59"} /-->
+<!-- /wp:navigation -->
+
+<!-- wp:cover {"url":"https://richiezz.files.wordpress.com/2020/06/istockphoto-519368058-170667a.jpg","id":31,"dimRatio":20,"customOverlayColor":"#000000","focalPoint":{"x":"0.50","y":"0.70"},"minHeight":620,"align":"full","className":"is-style-bottom-wave"} -->
+<div class="wp-block-cover alignfull has-background-dim-20 has-background-dim is-style-bottom-wave" style="background-image:url(https://richiezz.files.wordpress.com/2020/06/istockphoto-519368058-170667a.jpg);background-color:#000000;background-position:50% 70%;min-height:620px"><div class="wp-block-cover__inner-container"><!-- wp:jetpack/layout-grid {"addGutterEnds":false,"column1DesktopSpan":10,"column1DesktopOffset":1,"column1TabletSpan":8,"column1MobileSpan":4,"column2DesktopOffset":1,"className":"column1-desktop-grid__span-10 column1-desktop-grid__start-2 column1-desktop-grid__row-1 column1-tablet-grid__span-8 column1-tablet-grid__row-1 column1-mobile-grid__span-4 column1-mobile-grid__row-1"} -->
+<div class="wp-block-jetpack-layout-grid alignfull column1-desktop-grid__span-10 column1-desktop-grid__start-2 column1-desktop-grid__row-1 column1-tablet-grid__span-8 column1-tablet-grid__row-1 column1-mobile-grid__span-4 column1-mobile-grid__row-1 wp-block-jetpack-layout-gutter__nowrap"><!-- wp:jetpack/layout-grid-column -->
+<div class="wp-block-jetpack-layout-grid-column wp-block-jetpack-layout-grid__padding-none"><!-- wp:paragraph {"align":"center","className":"margin-bottom-none","style":{"color":{"text":"#ffffff"},"typography":{"fontSize":80}}} -->
+<p class="has-text-align-center margin-bottom-none has-text-color" style="font-size:80px;color:#ffffff"><strong>Forelle Online</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center","className":"margin-top-none","style":{"color":{"text":"#ffffff"}}} -->
+<p class="has-text-align-center margin-top-none has-text-color" style="color:#ffffff">Alles rund um die Forelle !</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:jetpack/layout-grid-column --></div>
+<!-- /wp:jetpack/layout-grid --></div></div>
+<!-- /wp:cover -->
+
+<!-- wp:columns {"align":"wide"} -->
+<div class="wp-block-columns alignwide"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:heading {"level":5} -->
+<h5><a href="https://richiezz.wordpress.com/?page_id=55">Verschiedenes Angelzubehör</a></h5>
+<!-- /wp:heading -->
+
+<!-- wp:image {"id":33,"sizeSlug":"large"} -->
+<figure class="wp-block-image size-large"><img src="https://richiezz.files.wordpress.com/2020/06/fishing-1572408__340.jpg?w=510" alt="" class="wp-image-33"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:heading {"level":5} -->
+<h5><a href="https://richiezz.wordpress.com/?page_id=47">Leckere Forellen - Rezepte</a></h5>
+<!-- /wp:heading -->
+
+<!-- wp:image {"id":32,"sizeSlug":"large"} -->
+<figure class="wp-block-image size-large"><img src="https://richiezz.files.wordpress.com/2020/06/trout-2900325__340.jpg?w=510" alt="" class="wp-image-32"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:heading {"level":5} -->
+<h5><a href="https://richiezz.wordpress.com/?page_id=59">Wo Angelt man Forlellen</a></h5>
+<!-- /wp:heading -->
+
+<!-- wp:image {"id":30,"sizeSlug":"large"} -->
+<figure class="wp-block-image size-large"><img src="https://richiezz.files.wordpress.com/2020/06/istockphoto-1146124717-170667a.jpg?w=509" alt="" class="wp-image-30"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:spacer {"height":32,"className":"desktop-only"} -->
+<div style="height:32px" aria-hidden="true" class="wp-block-spacer desktop-only"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:separator -->
+<hr class="wp-block-separator"/>
+<!-- /wp:separator -->
+
+<!-- wp:spacer {"height":64,"className":"desktop-only"} -->
+<div style="height:64px" aria-hidden="true" class="wp-block-spacer desktop-only"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:group {"align":"full","backgroundColor":"background-light"} -->
+<div class="wp-block-group alignfull has-background-light-background-color has-background"><div class="wp-block-group__inner-container"><!-- wp:spacer {"height":80,"className":"desktop-only"} -->
+<div style="height:80px" aria-hidden="true" class="wp-block-spacer desktop-only"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:columns {"align":"wide"} -->
+<div class="wp-block-columns alignwide"><!-- wp:column -->
+<div class="wp-block-column"></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:heading {"level":5} -->
+<h5>Kontaktiere uns</h5>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"left"} -->
+<p class="has-text-align-left"></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:spacer {"height":80,"className":"desktop-only"} -->
+<div style="height:80px" aria-hidden="true" class="wp-block-spacer desktop-only"></div>
+<!-- /wp:spacer --></div></div>
+<!-- /wp:group -->
